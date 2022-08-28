bbcf8551abaf95956e4a1320f47a3084300f7576
NastiHluhan <0506167197more@gmail.com>
2022-08-28 22:52:37 +0300

    add swiper

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper__btn--next',
+    prevEl: '.swiper__btn--prev',
+  },
+});
