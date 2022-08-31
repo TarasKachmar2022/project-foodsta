212a0fcee0b80971df1530eb49be0a7290a57c58
NastiHluhan <0506167197more@gmail.com>
2022-08-31 11:02:05 +0300

    add jsssss

--- src/js/animation.js
+++ src/js/animation.js
@@ -6,3 +6,13 @@ $(document).ready(function () {
     $('body,html').animate({ scrollTop: top }, 1500);
   });
 });
+
+$(document).ready(function () {
+  $('#menu-mobile').on('click', 'a', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
+s;
