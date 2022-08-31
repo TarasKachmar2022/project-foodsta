cde65947c72aa79617dda9431030b51972f37ed9
Rostyslav <rossgaluzinskiy@gmail.com>
2022-08-31 11:35:41 +0300

    add js

--- src/js/animation.js
+++ src/js/animation.js
@@ -15,4 +15,12 @@ $(document).ready(function () {
     $('body,html').animate({ scrollTop: top }, 1500);
   });
 });
-s;
+
+$(document).ready(function () {
+  $('#menu-footer').on('click', 'a', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
